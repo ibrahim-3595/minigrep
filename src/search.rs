fd3c07682a06c702516a98cbbfc73cfe2ce683b5
Ibrahim <ibrahim.haji.3595@gmail.com>
2025-11-19 20:03:24 +0530

    updated search feature for colorized grep matching

--- src/search.rs
+++ src/search.rs
@@ -32,16 +32,17 @@ pub fn recursive_search(
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<String> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .map(|line| {
+            line.replace(
+                query,
+                &format!("\x1b[31m{}\x1b[0m", query), // RED highlight
+            )
+        })
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
